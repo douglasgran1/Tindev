9fcd5482f9233b78a81dcc6b5453ad561b3ffb32
Douglas Pimentel <douglas.pimentel@hotmail.com>
2020-10-16 21:39:36 -0300

    Backend node

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://douglasgran:158259ae@cluster0.62wng.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
